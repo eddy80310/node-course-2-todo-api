4a9e0aa21e310b018e7aeeb7577ab9e491bcef45
eddy80310 <eddy_hung9999@hotmail.com>
2017-03-26 17:12:43 +1000

    Add GET / todods route

--- server/models/todo.js
+++ server/models/todo.js
@@ -2,17 +2,17 @@ var mongoose = require('mongoose')
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String, 
-        required: true, 
-        minlength: 1, 
+        type: String,
+        required: true,
+        minlength: 1,
         trim: true
-    }, 
+    },
     completed: {
-        type: Boolean, 
+        type: Boolean,
         default: false
-    }, 
+    },
     completedAt: {
-        type: Number, 
+        type: Number,
         default: null
     }
 });
